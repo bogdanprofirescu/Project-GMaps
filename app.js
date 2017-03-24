f797965bab241c2dac78f1629d52d293f2f44e3f
bogdanprofirescu <bogdanprofirescu@yahoo.com>
2017-03-24 13:24:57 +0200

    Location change at the right zoom works, with identification of places

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ document.getElementById("close_btn").addEventListener('click',closeMenu,false);
 
 //this function is a candidate for knockout.js
 window.onload= function() {
-document.getElementById("areaChoice").addEventListener('change', focusChosenArea);
-document.getElementById("typeOfPlace").addEventListener('change', focusChosenArea);
+// document.getElementById("areaChoice").addEventListener('change', focusChosenArea);
+// document.getElementById("typeOfPlace").addEventListener('change', focusChosenArea);
+document.getElementById("searchBttn").addEventListener('click', focusChosenArea);
 };
 
 
@@ -15,6 +16,7 @@ var navigationBar=document.getElementById("navigationBar");
 var main=document.getElementById("main");
 var start_btn_background=document.getElementById("start_btn_background");
 var sidenav_items=document.getElementsByClassName("sidenav")[0];
+var divSideNavPlaces = document.getElementById('places');
 
 //open and close menu functions change the sidebar width & hide/show
 // the main menu button
@@ -24,12 +26,14 @@ function openMenu() {
   if (window.innerWidth<700) navigationBar.style.width = "160px";
       else navigationBar.style.width = "235px";
   sidenav_items.style.display="";
+  navigationBar.style.padding= "3px";
   }
 
 function closeMenu() {
   start_btn_background.style.display="";
   // sidenav_items.style.display="none";
     navigationBar.style.width = "0";
+    navigationBar.style.padding= "0";
 }
 
 var map;
@@ -40,19 +44,8 @@ var infowindow;
 var newCenter;
 var destination=document.getElementById("areaChoice");
 var typeOfPlaceSelected=document.getElementById("typeOfPlace");
+var range=document.getElementById("range");
 var mapBounds;
-
-
-var points_of_interest=[
-  {title:'Piata Sfatului', location:{lat: 45.642024, lng: 25.589116}},
-  {title:'Black Church', location:{lat:45.640879, lng:25.587810}},
-  {title:'Bran Castle', location:{lat:45.514902, lng:25.367159}},
-  {title:'Poiana Brasov', location:{lat:45.595337,lng: 25.552903}},
-  {title:'Cantacuzino Castle', location:{lat:45.414050,lng:25.542612}},
-  {title: 'Peles Castle', location:{lat:45.359984, lng: 25.542644}},
-  {title: 'Brasovia Citadel',location:{lat:45.649319,lng:25.591889}},
-  {title:'Rasnov Fortress', location:{lat:45.590236,lng:25.469437}}
-];
 
 function initMap()
 {
@@ -63,8 +56,8 @@ if (typeof google === 'undefined') alert("google api not loaded");
 //onerror function does not work
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 45.642024, lng: 25.589116},
-    zoom: 13,
+    // center: {lat: 45.642024, lng: 25.589116},
+    zoom: 12,
       mapTypeControlOptions: {
               style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
               position: google.maps.ControlPosition.TOP_CENTER
@@ -77,170 +70,104 @@ if (typeof google === 'undefined') alert("google api not loaded");
           streetViewControl: false,
           fullscreenControl: true
   });
+
   mapBounds = new google.maps.LatLngBounds();
+  mapBoundsReset= new google.maps.LatLngBounds();
   infowindow= new google.maps.InfoWindow();
-  addInitialMarkers();
-// adding locations
-//TODO - should we try to load markers from a text file via an iFrame?
-//creating the markers and storing them in a markers array
-};
-// ----END OF INITMAP FUNCTION----
-function addInitialMarkers()
-{
-  for (var i=0;i<points_of_interest.length;i++)
+  focusChosenArea();
+  };
+
+
+  function focusChosenArea()
       {
-        createMarker(points_of_interest[i],i);
-        // console.log(typeof(points_of_interest[i]));
-
-            }
-}
-
-
-// function addMarkers()
-// {
-//   var bounds = new google.maps.LatLngBounds();
-//   // var largeInfowindow = new google.maps.InfoWindow();
-//
-//   for (var i=0;i<points_of_interest.length;i++)
-//     {
-//       var marker_location=points_of_interest[i].location;
-//       var marker_title=points_of_interest[i].title;
-//       var marker = new google.maps.Marker({
-//             position: marker_location,
-//             title: marker_title,
-//             animation: google.maps.Animation.DROP,
-//             // icon: defaultIcon,
-//             id: i
-//           });
-//       markers_set.push(marker);
-//       marker.addListener('click', function() {
-//         populateInfoWindow(this, infowindow);
-//       });
-//       markers_set[i].setMap(map);
-//       bounds.extend(markers_set[i].position);
-//     }
-//     map.fitBounds(bounds);
-// }
-
-
-function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-          });
-          var streetViewService = new google.maps.StreetViewService();
-          var radius = 50;
-
-          // position of the streetview image, then calculate the heading, then get a
-        // panorama from that and set the options
-            function getStreetView(data, status) {
-                if (status == google.maps.StreetViewStatus.OK) {
-                  var nearStreetViewLocation = data.location.latLng;
-                  var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
-                  infowindow.setContent("<div>" + marker.title + "</div><div id=\"pano\"></div>");
-
-                  var panoramaOptions = {
-                    position: nearStreetViewLocation,
-                  pov: {
-                    heading: heading,
-                    pitch: 20
-                  }
-                };
-                var panorama = new google.maps.StreetViewPanorama(
-                  document.getElementById('pano'), panoramaOptions);
-                } else {
-                  infowindow.setContent('<div>' + marker.title + '</div>' +'<div>No Street View Found</div>');
-                }
-              }
-    // Use streetview service to get the closest streetview image within
-    // 50 meters of the markers position
-    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-    // Open the infowindow on the correct marker.
-      infowindow.open(map, marker);
-        }
-      }
-
-
-      function focusChosenArea()
-      {
-
         var geocoder = new google.maps.Geocoder();
         geocoder.geocode(
           { address: destination.value},
            function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
                      {
-                       map.setCenter(results[0].geometry.location);
-                       findPlaces(results[0].geometry.location, typeOfPlaceSelected.value);
+                      //  map.setCenter(results[0].geometry.location);
+                       findPlaces(results[0].geometry.location, typeOfPlaceSelected.value,range.value);
                             };
             }
-             else {
-              window.alert('We could not find that location - try entering a more' +
-                  ' specific place.');
-            }
+                   else {
+                    window.alert('We could not find that location - try entering a more' +
+                        ' specific place.');
+                  }
           });
       }
 
-      function findPlaces(newCenter,typeOfPlaceSelected) {
-        console.log(newCenter);
+      function findPlaces(newCenter,typeOfPlaceSelected,rangeValue) {
+        console.log(rangeValue);
         var service = new google.maps.places.PlacesService(map);
 
-        if (typeOfPlaceSelected=='castle')
-                        service.nearbySearch({
+            service.nearbySearch({
                                 location: newCenter,
-                                radius: 50000,
+                                radius: rangeValue,
                                 keyword: typeOfPlaceSelected,
                                 type: 'establishment'
-                              }, callback)
-
-                      else   service.nearbySearch({
-                              location: newCenter,
-                              radius: 5000,
-                              type: typeOfPlaceSelected
-                            }, callback)
+                              }, callback);
 
               function callback(results, status) {
               if (status === google.maps.places.PlacesServiceStatus.OK)
                            {
                                     console.log(results.length);
                                     eraseMarkers();
+                                    clearList();
+                                    mapBounds=mapBoundsReset;
                                     for (var i = 0; i < results.length; i++)
                                             {
                                                 var newPlaceToMark={title: results[i].name, location:{lat: results[i].geometry.location.lat(), lng: results[i].geometry.location.lng()}};
-                                                // console.log(newPlaceToMark);
-                                                // console.log(newPlaceToMark.location.lat);
-                                                // console.log(newPlaceToMark.location.lng);
-                                                // createMarker(results[i],i);
+                                                // createListItem(newPlaceToMark);
                                                 createMarker(newPlaceToMark,i);
                                             }
+                                TransitionToNewLocation(newCenter);
                                 }
                           }
       }
+
+      function TransitionToNewLocation(newCenter) {
+              map.setCenter(newCenter);
+              map.fitBounds(mapBounds);
+              map.setZoom(12);
+              map.panTo(newCenter);
+              // only in this succesion I manage to center the map at the right zoom
+              console.log("zoom after new center:", map.getZoom());
+      }
+
+      function clearList()
+      {
+          console.log("clear list called..");
+          divSideNavPlaces.innerHTML="";
+      }
+
+          function createListItem(listItem)
+          {
+              var string;
+                string='<div class="placeDiv"><p class="placeName">'+listItem.title+'</p>'+
+                        '<img src=' +'"'+'http://maps.googleapis.com/maps/api/streetview?location='+
+                                  listItem.location.lat+','+
+                                  listItem.location.lng+'&size=80x92'+
+                                  '&key=AIzaSyARDaZozs7u65RbsBI4Xjwx7jJJ87iUAjY'+
+                                  '"></div>';
+
+            divSideNavPlaces.innerHTML = divSideNavPlaces.innerHTML + string;
+          }
+
+
+
             function eraseMarkers()
             {
-              console.log('#markers_set:', markers_set.length);
+              console.log('#markers_length:', markers_set.length);
               var array_size=markers_set.length;
               for (var i=0; i<markers_set.length; i++) markers_set[i].setMap(null);
               markers_set.splice(0,array_size);
-
-              console.log('#markers_set:', markers_set.length);
+              console.log('#markers_new length:', markers_set.length);
             }
 
 
             function createMarker(place,i) {
               var defaultIcon = makeMarkerIcon('0091ff');
-              // console.log(typeof(place));
-              // var placeLoc = {
-              //   lat: place.geometry.location.lat(),
-              //   lng: place.geometry.location.lng()
-              // };
-              // console.log(place.name);
-
               var marker = new google.maps.Marker({
                 map: map,
                 title: place.title,
@@ -251,11 +178,8 @@ function populateInfoWindow(marker, infowindow) {
               });
                 markers_set.push(marker);//add markers to the set
                 mapBounds.extend(marker.position);
-                map.fitBounds(mapBounds);
-                // var newInfowindow = new google.maps.InfoWindow();
+                // map.fitBounds(mapBounds);
                 google.maps.event.addListener(marker, 'click', function() {
-                // infowindow.setContent(place.name);
-                // infowindow.open(map, this);
                 populateInfoWindow(this, infowindow);
               });
             }
@@ -271,3 +195,44 @@ function populateInfoWindow(marker, infowindow) {
                 new google.maps.Size(21,34));
                 return markerImage;
               };
+            function populateInfoWindow(marker, infowindow) {
+                      // Check to make sure the infowindow is not already opened on this marker.
+                      if (infowindow.marker != marker) {
+                        infowindow.marker = marker;
+                        infowindow.setContent('');
+                        infowindow.open(map, marker);
+                        // Make sure the marker property is cleared if the infowindow is closed.
+                        infowindow.addListener('closeclick', function() {
+                          infowindow.marker = null;
+                        });
+                        var streetViewService = new google.maps.StreetViewService();
+                        var radius = 50;
+
+                      // position of the streetview image, then calculate the heading, then get a
+                      // panorama from that and set the options
+                      function getStreetView(data, status) {
+                              if (status == google.maps.StreetViewStatus.OK) {
+                                var nearStreetViewLocation = data.location.latLng;
+                                var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
+                                infowindow.setContent("<div>" + marker.title + "</div><div id=\"pano\"></div>");
+
+                                var panoramaOptions = {
+                                  position: nearStreetViewLocation,
+                                pov: {
+                                  heading: heading,
+                                  pitch: 20
+                                }
+                              };
+                              var panorama = new google.maps.StreetViewPanorama(
+                                document.getElementById('pano'), panoramaOptions);
+                              } else {
+                                infowindow.setContent('<div>' + marker.title + '</div>' +'<div>No Street View Found</div>');
+                              }
+                            }
+                  // Use streetview service to get the closest streetview image within
+                  // 50 meters of the markers position
+                     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+                  // Open the infowindow on the correct marker.
+                    infowindow.open(map, marker);
+                      }
+                    }
